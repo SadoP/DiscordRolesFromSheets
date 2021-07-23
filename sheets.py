9321357a8f4d42c0b5d81853019d71bcbacd4b77
Pascal Sado <37416439+SadoP@users.noreply.github.com>
2021-07-23 15:33:30 +0200

    converting from oauth to service account

--- sheets.py
+++ sheets.py
@@ -5,17 +5,18 @@ import os.path
 import traceback
 
 import pandas as pd
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from logger import logger
+
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+
 class SheetReader:
-    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    token_path = "token.json"
-    credentials_path = "credentials.json"
+    scopes = ['https://www.googleapis.com/auth/spreadsheets',
+              'https://www.googleapis.com/auth/drive']
+
+    service_account_file = "service_account.json"
     config_path = "config.json"
     creds = None
     sheet = None
@@ -23,26 +24,16 @@ class SheetReader:
 
     def __init__(self):
         self.config = self.get_config()
-        self.get_creds()
+        self.creds = self.get_creds()
         self.sheet = self.get_service().spreadsheets()
 
-    # Adapted from google's documentations:
-    # https://developers.google.com/sheets/api/quickstart/python
     def get_creds(self):
-        if os.path.exists(self.token_path):
-            self.creds = Credentials.from_authorized_user_file(self.token_path, self.scopes)
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(self.credentials_path, self.scopes)
-                self.creds = flow.run_local_server(port=0)
-            with open(self.token_path, 'w') as token:
-                token.write(self.creds.to_json())
+        credentials = service_account.Credentials.from_service_account_file(
+            self.service_account_file, scopes=self.scopes)
+        return credentials
 
     def get_service(self):
-        if self.creds:
-            return build('sheets', 'v4', credentials=self.creds)
+        return build('sheets', 'v4', credentials=self.creds)
 
     def get_config(self):
         if os.path.exists(self.config_path):
@@ -57,6 +48,7 @@ class SheetReader:
                 col, rem = divmod(col - 1, 26)
                 result[:0] = LETTERS[rem]
             return ''.join(result)
+
         self.get_creds()
         sheetId = self.config.get("sheetId")
         sheet = self.config.get("sheetName")
@@ -64,8 +56,8 @@ class SheetReader:
         roleColumns = [role.get("roleColumn") for role in self.config.get("roles")]
         roleIDs = [role.get("roleID") for role in self.config.get("roles")]
         informationRow = int(self.config.get("InformationRowStart"))
-        cols = [userIDColumn]+roleColumns
-        pdCols = ["userID"]+roleIDs
+        cols = [userIDColumn] + roleColumns
+        pdCols = ["userID"] + roleIDs
         try:
             result = self.sheet.values().get(spreadsheetId=sheetId, range=sheet).execute()
         except Exception as e:
@@ -73,9 +65,9 @@ class SheetReader:
             logger.error("error upon requesting data", e)
             return
         data = pd.DataFrame(result.get("values")[1:], columns=result.get("values")[0])
-        columns = [excel_style(i+1) for i in range(len(data.columns))]
+        columns = [excel_style(i + 1) for i in range(len(data.columns))]
         data.columns = columns
-        data = data.iloc[informationRow-2:].loc[:, cols]
+        data = data.iloc[informationRow - 2:].loc[:, cols]
         data.columns = pdCols
         data.loc[:, roleIDs] = data.loc[:, roleIDs] == self.config.get("roleConfirmationPhrase")
         data.index = data.loc[:, "userID"]
